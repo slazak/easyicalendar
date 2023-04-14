bde83b34a6273ed5d1292bfc380a2da54466525c
Piotr Donica <piotr.donica@moonjelly.app>
2023-04-14 17:09:28 +0200

    Added build config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://easyicalendar.com',
+  // base: '/easyicalendar',
   integrations: [vue()]
 });
